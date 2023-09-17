4abf06ce2abf861208f3d2828486748f91717e43
SaurabhManikeri <manikerisaurabh@gmail.com>
2023-09-17 10:41:50 +0530

    added hello wrold.c file

---
+++ #1_hello_world.c
@@ -0,0 +1,14 @@
+/*
+----------------------------    Hello World!   ------------------------------
+Problem Statement - Write a program to print Hello World!
+
+Sample Output : Hello World!
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
